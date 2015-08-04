bc60dd485d9c1366edee6e827f336d401abe7f81
Hua Zhang <billyzhang2010@gmail.com>
2015-08-04 13:42:37 -0700

    variable

---
+++ src/golang-book/chapter4/variable.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+    x := "hello world"
+    fmt.Printf("Go compiler is able to infer the type based on the literal value you assign the variable\n")
+    fmt.Printf("x=%s \n", x);
+
+}
