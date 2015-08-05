d1a4776cdf054a275aedcc95d27cc983b4c30969
Hua Zhang <billyzhang2010@gmail.com>
2015-08-05 15:29:48 -0700

    add a new variable out of function

--- src/golang-book/chapter4/variable.go
+++ src/golang-book/chapter4/variable.go
@@ -2,9 +2,12 @@ package main
 
 import "fmt"
 
+var dogName string = "Max"
+
 func main() {
     x := "hello world"
     fmt.Printf("Go compiler is able to infer the type based on the literal value you assign the variable\n")
     fmt.Printf("x=%s \n", x);
-
+    
+    fmt.Printf("dogName = %s\n", dogName)
 }
